43e9d406f5b3fcc29572cfebfd1e4aa430a8601c
Daniel Park <intheimageofyeshua@gmail.com>
2025-03-06 11:27:55 +0900

    User Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+user = User.find_by(email_address: "admin@test.com", role: :admin)
+
+if !user
+  User.create(email_address: "admin@test.com", password: "admin1234", role: :admin)
+end
