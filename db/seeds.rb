437474fb64e4b001195df08602aeb2c8e43b90c6
Daniel Park <intheimageofyeshua@gmail.com>
2025-03-06 21:59:50 +0900

    Create Post Show Page

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,10 +14,24 @@ if !user
   User.create(email_address: "admin@test.com", password: "admin1234", role: :admin)
 end
 
-post = Post.find_by(title: "The Glorious Mountain Ever!")
+post1 = Post.find_by(title: "The Glorious Mountain Ever!")
+post2 = Post.find_by(title: "The Glorious River Ever!")
+post3 = Post.find_by(title: "The Glorious Fruit Ever!")
 
-if !post
-  post = Post.create(title: "The Glorious Mountain Ever!",
-    body: "Some mountain")
-  post.banner_image.attach(io: File.open(Rails.root.join("public/mountain.jpg")), filename: "mountain.jpg")
+if !post1
+  post1 = Post.create(title: "The Glorious Mountain Ever!",
+    body: "Some mountain standing on earth...")
+  post1.banner_image.attach(io: File.open(Rails.root.join("public/mountain.jpg")), filename: "mountain.jpg")
+end
+
+if !post2
+  post2 = Post.create(title: "The Glorious River Ever!",
+    body: "Some river flows through the earth...")
+  post2.banner_image.attach(io: File.open(Rails.root.join("public/river.jpg")), filename: "river.jpg")
+end
+
+if !post3
+  post3 = Post.create(title: "The Glorious Fruit Ever!",
+    body: "Some fruit shines in the man and woman...")
+  post3.banner_image.attach(io: File.open(Rails.root.join("public/fruit.jpg")), filename: "river.jpg")
 end
